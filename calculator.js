a15f575d528c1001ae55e912c4372f677e92d908
Chris Skeens <cskee004@odu.edu>
2024-07-05 12:29:38 -0400

    Implemented calculator object to hold operands, operator, and result. Result is to be used if any additional calculations need to be handled.

--- calculator.js
+++ calculator.js
@@ -1,54 +1,92 @@
+// Goal: Add an event listener to the button types. DONE
+// Goal: Create an object that holds two operands and one operator. DONE
+// Goal: Create a function operate that takes the calculation object and then calls the function.
+// Goal: Wrap event listeners into a function
+
+
+
 const container = document.querySelector("#container");
-const display = document.querySelector("#display");
 const operand = document.querySelectorAll(".operand");
-const operator = document.querySelector(".operator");
+const operator = document.querySelectorAll(".operator");
+const clear = document.querySelectorAll(".clear");
+const display = document.querySelector("#display");
 
-operand.forEach(element => element.addEventListener('click', event => {
-    console.log(event.target.getAttribute("data-el"));
-  }));
+function calcObj(op1, op2, operator) {
+    this.op1 = op1;
+    this.op2 = op2;
+    this.operator = operator;
+    result = 0;
+}
 
+clear.forEach(element => element.addEventListener('click', event => { 
+    console.log(event.target.getAttribute("data-element"));
 
+    // Delete calcObj on clear button
+}));
 
+newFunction();
 
+function newFunction() {
+    operator.forEach(element => element.addEventListener('click', event => {
+        console.log(event.target.getAttribute("data-element"));
+    }));
 
+    operand.forEach(element => element.addEventListener('click', event => {
+        console.log(Number(event.target.getAttribute("data-element")));
+    }));
+}
 
+function setUp() {
+    let test = new calcObj(1, 2, '+');
+    operate(test);
+}
 
+setUp();
 
+function operate(arr) {
 
-
-
-
-
-function operate(x, y, operator) {
-
-    switch (operator) {
+    switch (arr.operator) {
         case '+':
-            add(x, y);
+            add(arr);
             break;
         case '-':
-            subtract(x, y);
+            subtract(arr);
             break;
         case '*': 
-            multiply(x, y);
+            multiply(arr);
             break;
         case '/':
-            divide(x, y);
+            divide(arr);
             break;
     }
 }
 
-function add(x, y) {
-    return x + y;
+
+function add(arr) {
+    let c = arr.op1 + arr.op2;
+    arr.result = c;
+    display.innerHTML = c;
+    return c;
 }
 
-function subtract(x, y) {
-    return x - y;
+function subtract(arr) {
+    let c = arr.op1 - arr.op2;
+    arr.result = c;
+    display.innerHTML = c;
+    return c;
 }
 
-function multiply(x, y) {
-    return x * y;
+function multiply(arr) {
+    let c = arr.op1 * arr.op2;
+    arr.result = c;
+    display.innerHTML = c;
+    return c;
 }
 
-function divide(x, y) {
-    return x / y;
+function divide(arr) {
+    let c = arr.op1 / arr.op2;
+    arr.result = c;
+    display.innerHTML = c;
+    return c;
 }
+
