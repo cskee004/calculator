a8f251c6fdfbe9e18befea952edcb88494561df7
Chris Skeens <cskee004@odu.edu>
2024-07-06 16:01:38 -0400

    Added last key hit to the display. Added case for dividing by 0.

--- calculator.js
+++ calculator.js
@@ -5,96 +5,96 @@ const clear = document.querySelectorAll(".clear");
 const display = document.querySelector("#display");
 const equal = document.querySelectorAll(".equal")
 
-let calcArr = [2, 2, 2, '/', 2, 2, 2];
+let calcArr = [];
 
 //----------------------------------------------------------------------------->
 clear.forEach(element => element.addEventListener('click', event => { 
-    // console.log(event.target.getAttribute("data-element"));
     window.location.reload();
 }));
 //----------------------------------------------------------------------------->
 equal.forEach(element => element.addEventListener('click', event => { 
-    // console.log(event.target.getAttribute("data-element"));
     operate(calcArr);
 }));
 //----------------------------------------------------------------------------->
 operator.forEach(element => element.addEventListener('click', event => {
-    // console.log(event.target.getAttribute("data-element"));
     let input = event.target.getAttribute("data-element");
+    display.innerHTML = input;
     calcArr.push(input);
 }));
 //----------------------------------------------------------------------------->
 operand.forEach(element => element.addEventListener('click', event => {
-    // console.log(Number(event.target.getAttribute("data-element")));
     let input = Number(event.target.getAttribute("data-element"));
+    display.innerHTML = input;
     calcArr.push(input);
     
 }));
 //----------------------------------------------------------------------------->
-
 function operate(calcArr) {
         let op = '';
         let op0 = 0;
         let op1 = 0;
         let tempValue = 0;
+
         for (let i = 0; i < calcArr.length; ++i) {
             if (typeof calcArr[i] !== 'number') {
+                
                 op = calcArr[i]; 
-                // Encounted an operator, therefore the current value of op0 represents first operand.  
-                // Reset tempvalue to 0 for the next operand value.
                 op0 = tempValue;
                 tempValue = 0;
             }
             else {
+                
                 tempValue += calcArr[i];
             }
         
             op1 = tempValue;
         }
+        
         solve(op0, op, op1);
 }
-
+//----------------------------------------------------------------------------->
 function solve(op0, op, op1) {
-    switch (op) {
-        case '+':
-            console.log(add(op0, op1));
-            display.innerHTML = add(op0, op1);
-            break;
-        case '-':
-            console.log(subtract(op0, op1));
-            display.innerHTML = subtract(op0, op1);
-            break;
-        case '*':
-            console.log(multiply(op0, op1));
-            display.innerHTML = multiply(op0, op1);
-            break;
-        case "/": 
-            console.log(divide(op0, op1));
-            display.innerHTML = divide(op0, op1);
-            break;
-
+    if (op == '/' && op1 == 0){
+        display.innerHTML = "LOL"
+        return;
+    }
+    else {
+        switch (op) {
+            case '+':
+                console.log(add(op0, op1));
+                display.innerHTML = add(op0, op1);
+                break;
+            case '-':
+                console.log(subtract(op0, op1));
+                display.innerHTML = subtract(op0, op1);
+                break;
+            case '*':
+                console.log(multiply(op0, op1));
+                display.innerHTML = multiply(op0, op1);
+                break;
+            case "/": 
+                console.log(divide(op0, op1));
+                display.innerHTML = divide(op0, op1);
+                break;
+        }
     }
 }
 //----------------------------------------------------------------------------->
-
 function add(op0, op1) {
     let c = op0 + op1;
     return c;
 }
 //----------------------------------------------------------------------------->
-
 function subtract(op0, op1) {
     let c = op0 - op1;
     return c;
 }
 //----------------------------------------------------------------------------->
-
 function multiply(op0, op1) {
     let c = op0 * op1;
     return c;
 }
 //----------------------------------------------------------------------------->
-
 function divide(op0, op1) {
     let c = op0 / op1;
     return c;
